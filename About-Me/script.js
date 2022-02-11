0b5f4177da4b7cb509a94b25d5cbae4023c2cd9b
Austin Coding Academy <austincodingacademy@Administrators-MacBook-Pro.local>
2022-02-10 20:48:27 -0600

    uploading portfolio

---
+++ About-Me/script.js
@@ -0,0 +1,10 @@
+const disappear = (element) => {
+
+    // console.log("element was clicked " + element.id)
+
+    // console.log(element.style)
+
+    document.getElementById(element.id).style.display = "none"
+
+
+}
